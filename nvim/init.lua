d3c713fbc3413c986ef5f81bc53b0ed9420994a5
joao vitor v <joaovitorzv@outlook.com>
2021-11-21 18:53:16 -0300

    gitwatch got something for us

--- nvim/init.lua
+++ nvim/init.lua
@@ -296,7 +296,7 @@ local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
 
 -- Enable the following language servers
-local servers = { 'clangd', 'rust_analyzer', 'pyright', 'tsserver', 'bashls', 'eslint_d' }
+local servers = { 'clangd', 'rust_analyzer', 'pyright', 'tsserver', 'bashls' }
 for _, lsp in ipairs(servers) do
   nvim_lsp[lsp].setup {
     on_attach = on_attach,
