cdf88360fc001d7f86842f58c088fde9dbc79fec
joao vitor v <joaovitorzv@outlook.com>
2021-11-21 15:24:12 -0300

    gitwatch got something for us

--- nvim/init.lua
+++ nvim/init.lua
@@ -58,6 +58,7 @@ end)
 vim.o.tabstop = 2
 vim.o.softtabstop = 2
 vim.o.shiftwidth = 2
+vim.o.expandtab = true
 
 --Incremental live completion (note: this is now a default on master)
 vim.o.inccommand = 'nosplit'
