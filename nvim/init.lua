3cc5c22ac2a49368230aaa0e9913c09e220dd493
joao vitor v <joaovitorzv@outlook.com>
2021-11-21 19:30:12 -0300

    gitwatch got something for us

--- nvim/init.lua
+++ nvim/init.lua
@@ -61,7 +61,7 @@ require('packer').startup(function()
     end,
     requires = {"nvim-lua/plenary.nvim", "neovim/nvim-lspconfig"}
   })
-
+  use 'jose-elias-alvarez/nvim-lsp-ts-utils'
 end)
 
 -- Tabs
