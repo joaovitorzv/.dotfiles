2b63d7eca00f61e138b4cd43883665b2ca241e3a
joao vitor v <joaovitorzv@outlook.com>
2021-11-21 15:00:59 -0300

    gitwatch got something for us

--- nvim/init.lua
+++ nvim/init.lua
@@ -292,9 +292,7 @@ local null_ls = require 'null-ls'
 local sources = { 
 		null_ls.builtins.formatting.prettier,
 		null_ls.builtins.formatting.eslint,
-		null_ls.builtins.diagnostics.eslint.with({
-			prefer_local = "node_modules/@eslint/eslintrc"
-		})
+		null_ls.builtins.diagnostics.eslint
 }
 null_ls.config({ sources = sources })
 
