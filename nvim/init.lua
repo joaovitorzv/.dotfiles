02351637c6556e283482d7e717ad258f1c5fcff4
joao vitor v <joaovitorzv@outlook.com>
2021-11-22 11:22:52 -0300

    gitwatch got something for us

--- nvim/init.lua
+++ nvim/init.lua
@@ -294,8 +294,8 @@ capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
 local null_ls = require "null-ls"
 
 local sources = {
-  null_ls.builtins.diagnostics.eslint_d, 
-  null_ls.builtins.code_actions.eslint_d,
+  null_ls.builtins.diagnostics.eslint, 
+  null_ls.builtins.code_actions.eslint,
   null_ls.builtins.formatting.prettier
 }
 
