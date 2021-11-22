985bd7176d832165c4a96389bf16d3d2013c6b50
joao vitor v <joaovitorzv@outlook.com>
2021-11-22 12:31:00 -0300

    gitwatch got something for us

--- nvim/init.lua
+++ nvim/init.lua
@@ -332,7 +332,7 @@ for _, lsp in ipairs(servers) do
         vim.api.nvim_buf_set_keymap(bufnr, 'n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
         vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>q', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
         -- vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>so', [[<cmd>lua require('telescope.builtin').lsp_document_symbols()<CR>]], opts)
-        vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>so', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
+        vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>so', '<cmd>lua vim.lsp.buf.formatting_sync()<CR>', opts)
 
         local ts_utils = require("nvim-lsp-ts-utils")
         -- defaults
