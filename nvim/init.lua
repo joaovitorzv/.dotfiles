c234345bb3d1c41c3ff30837b399be8a0108d91d
joao vitor v <joaovitorzv@outlook.com>
2021-11-22 08:33:00 -0300

    gitwatch got something for us

--- nvim/init.lua
+++ nvim/init.lua
@@ -381,6 +381,10 @@ for _, lsp in ipairs(servers) do
       end,
       capabilities = capabilities
     } 
+  elseif lsp == 'null-ls' then
+    nvim_lsp[lsp].setup {
+      debug = true
+    }
   else
     nvim_lsp[lsp].setup {
       on_attach = on_attach,
