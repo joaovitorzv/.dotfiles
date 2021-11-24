2d9a4bcf3fa4b7654be35e5d6a09b49e2bcf32d9
joao vitor v <joaovitorzv@outlook.com>
2021-11-24 17:41:07 -0300

    gitwatch got something for us

--- nvim/init.lua
+++ nvim/init.lua
@@ -152,6 +152,8 @@ vim.api.nvim_set_keymap('n', '<leader>!q', '<cmd> q<CR>', { noremap = true })
 -- Source init.lua
 vim.api.nvim_set_keymap('n', '<leader>up', '<cmd>so ~/dotfiles/nvim/init.lua<CR>', { noremap = true })
 
+
+
 -- Moving around
 vim.api.nvim_set_keymap('n', '<leader>h', '<cmd> wincmd h<CR>', { noremap = true })
 vim.api.nvim_set_keymap('n', '<leader>j', '<cmd> wincmd j<CR>', { noremap = true })
