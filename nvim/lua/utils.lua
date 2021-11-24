f9e3ce84e0b12f8503d39170be3075beed6d1ba7
joao vitor v <joaovitorzv@outlook.com>
2021-11-24 17:51:11 -0300

    gitwatch got something for us

--- nvim/lua/utils.lua
+++ nvim/lua/utils.lua
@@ -1,7 +1,6 @@
 local api = vim.api
 
 local get_options = function (custom_opts)
-  print(vim.inspect(custom_opts))
   local options = { noremap = true, silent = true }
   if custom_opts then
     options = vim.tbl_extend("force", options, custom_opts)
@@ -12,7 +11,6 @@ end
 local M = {}
 
 M.command = function (mode, keymap, source, opts)
-  print(vim.inspect(source))
   api.nvim_set_keymap(mode, keymap, source, get_options(opts))
 end
 
