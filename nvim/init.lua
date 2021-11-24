1ae95d69eb60fe8447b1b8f80bca7f08020f8b1b
joao vitor v <joaovitorzv@outlook.com>
2021-11-24 14:44:22 -0300

    gitwatch got something for us

--- nvim/init.lua
+++ nvim/init.lua
@@ -200,7 +200,7 @@ vim.g.indent_blankline_char_highlight = 'LineNr'
 vim.g.indent_blankline_show_trailing_blankline_indent = false
 vim.g.indent_blankline_show_first_indent_level = false
 
-vim.o.NERDTreeShowHidden = 1
+local NERDTreeShowHidden = 1
 
 -- Gitsigns
 require('gitsigns').setup {
