237a1f81919c812f2300f35c07115ee464e2360b
joao vitor v <joaovitorzv@outlook.com>
2021-11-25 19:10:24 -0300

    gitwatch got something for us

--- nvim/lua/lsp/init.lua
+++ nvim/lua/lsp/init.lua
@@ -28,4 +28,4 @@ end
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
 
-require('lsp.lua').setup(on_attach)
+require('sumneko.lua').setup(on_attach)
