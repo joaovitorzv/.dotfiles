d38e70551bfa6723c1e589c20123ecea684c7d59
joao vitor v <joaovitorzv@outlook.com>
2021-11-25 19:20:38 -0300

    gitwatch got something for us

--- nvim/lua/lsp/init.lua
+++ nvim/lua/lsp/init.lua
@@ -1,6 +1,4 @@
 -- LSP settings
-
-print('xd')
 local lspconfig = require 'lspconfig'
 local on_attach = function(_, bufnr)
   vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
