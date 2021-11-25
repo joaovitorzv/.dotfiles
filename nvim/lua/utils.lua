3c854d0ec6c1e0253f4798b13c1ba4e031c5d037
joao vitor v <joaovitorzv@outlook.com>
2021-11-25 11:12:51 -0300

    gitwatch got something for us

--- nvim/lua/utils.lua
+++ nvim/lua/utils.lua
@@ -15,7 +15,7 @@ M.map = function (mode, keymap, source, opts)
 end
 
 M.buf_map = function (bufnr, mode, keymap, source, opts)
-  api.nvim_set_buf_keymap(bufnr or 0, mode, keymap, source, get_options(opts))
+  print(vim.inspect(api.nvim_set_buf_keymap(bufnr or 0, mode, keymap, source, get_options(opts))))
 end
 
 return M
