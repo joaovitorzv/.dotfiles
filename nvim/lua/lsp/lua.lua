0da2422c21270b4cec459da86a04f894eb41a1b0
joao vitor v <joaovitorzv@outlook.com>
2021-11-25 17:08:54 -0300

    gitwatch got something for us

--- nvim/lua/lsp/lua.lua
+++ nvim/lua/lsp/lua.lua
@@ -18,7 +18,6 @@ local runtime_path = vim.split(package.path, ";")
 table.insert(runtime_path, "lua/?.lua")
 table.insert(runtime_path, "lua/?/init.lua")
 
-
 local settings = {
   Lua = {
     runtime = { version = "LuaJIT", path = runtime_path },
